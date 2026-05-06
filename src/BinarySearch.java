7b21b129e19e8821b062209395860e5d16adac44
Shreynaik1 <shreynaik315@gmail.com>
2026-05-06 13:25:23 +0530

    binarysearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,5 +1,28 @@
-public class BinarySearch {
+public class BinarySearch{
     public static void main(String[] args) {
+        int[] arr={1,3,4,7,8,9};
+        int target=129;
+      int ans=BS(arr,target);
+        System.out.println("element found at : " + ans);
+    }
+    static int BS(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
 
+
+        while(start<=end){
+            int mid=start + (end-start)/2;
+            if(target > arr[mid]){
+                start=mid+1;
+            }
+            else if(target < arr[mid]){
+                end=mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+
+        return -1;
     }
 }
